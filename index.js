445d1de99c3aa6eb0103a1a4fb7c278f7c97eeda
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-15 14:29:07 +0300

    added quotes to JS

---
+++ index.js
@@ -0,0 +1,16 @@
+const button = document.querySelector("#btn"); 
+const par = document.querySelector("showQuote"); 
+
+const quotes = ["You must be the change you wish to see in the world, - Mahatma Gandhi", 
+"The only thing we have to fear is fear itself, - Franklin D. Roosevelt", 
+"Do one thing every day that scares you. -Eleanor Roosevelt", 
+"Well done is better than well said, - Benjamin Franklin", 
+"Be yourself; everyone else is already taken, - Oscar Wilde", 
+"It is during our darkest moments that we must focus to see the light, - Aristotle", 
+"The purpose of our lives is to be happy, - Dalai Lama", 
+"You only live once, but if you do it right, once is enough, - Mae West", 
+"Many of life's failures are people who did not realize how close they were to success when they gave up, - Thomas A. Edison", 
+"When you undervalue what you do, the world will undervalue who you are, - Oprah Winfrey", 
+"Believe you can and you're halfway there, - Theodore Roosevelt", 
+"I would rather die of passion than of boredom, Vincent van Gogh"
+]; 
